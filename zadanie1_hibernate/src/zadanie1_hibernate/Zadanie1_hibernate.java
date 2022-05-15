7c277d878155bdd5f0aae3bb4042bec09056dd06
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-05-15 17:05:27 +0200

    add method createEmployee, delete/get/updateEmployeeById and getEmployee in Zadanie1_hibernate.java

--- zadanie1_hibernate/src/zadanie1_hibernate/Zadanie1_hibernate.java
+++ zadanie1_hibernate/src/zadanie1_hibernate/Zadanie1_hibernate.java
@@ -1,21 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package zadanie1_hibernate;
 
-/**
- *
- * @author Adam
- */
+import pojos.Pracownicy;
+import service.EmployeeService;
+
+
 public class Zadanie1_hibernate {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         
     }
     
+    private static void createEmployee(EmployeeService employeeService) {
+        employeeService.createEmployee(getEmployee());
+    }
+    
+    private static void deleteEmployeeById(EmployeeService employeeService) {
+		employeeService.deleteEmployeeById(1);
+	}
+    
+    private static Pracownicy getEmployee(){
+		Pracownicy employee= new Pracownicy();
+		employee.setPracImie("Adam");
+		employee.setPracNazwisko("Nowak");
+                employee.setPracWiek(22);
+                employee.setPracNrTelefonu("123456789");
+                employee.setPracEmail("adam@op.pl");
+		
+		return employee;
+	}
+
+    private static void getEmployeebyId(EmployeeService employeeService) {
+        Pracownicy employee = employeeService.getEmployeebyId(1);
+		System.out.println(employee.getPracId()+" "+employee.getPracImie()+" "+employee.getPracNazwisko());
+    }
+    
+    private static void updateEmployeeById(EmployeeService employeeService) {
+		employeeService.updateEmployeeById(5,"Maria","Kowalska",21,"234567891","maria.kowalska@op.pl");
+	}
 }
